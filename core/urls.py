fb7ad31b2607d8c2394e340ffb3e30128dfa9e65
Gabriel <gabaofogo@hotmail.com>
2018-11-19 09:26:23 -0300

    Primeira situação filtrando via Javascript concluida

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from core.views import home, pesquisa
+from core.views import home, pesquisa, home_javascript, pesquisa_javascript
 
 urlpatterns = [
     path('', home),
     path('pesquisa/', pesquisa),
+    path('home-javascript/', home_javascript),
+    path('pesquisa/curso/<str:nome>', pesquisa_javascript),
 ]
--- core/views.py
+++ core/views.py
@@ -14,9 +14,33 @@ def pesquisa(request):
     cursos = Curso.objects.all()
 
     # Atribuo a variavel 'curso_pesquisado' o valor pesquisado no formulário que agora está na URL
-    # lembrando que 'nomeInstituicao' é o atributo 'name' do input do formulário e que virou
+    # lembrando que 'nomeCurso' é o atributo 'name' do input do formulário e que virou
     # o campo na URL
-    curso_pesquisado = request.GET.get('nomeInstituicao')
+    curso_pesquisado = request.GET.get('nomeCurso')
+
+    # Se existir o valor na pesquisa
+    if curso_pesquisado is not None:
+        # Então filtre os cursos de acordo pelo seu nome
+        cursos = cursos.filter(nome__icontains=curso_pesquisado)
+
+    context = {
+        'cursos': cursos
+    }
+
+    return render(request, 'pesquisa.html', context)
+
+
+def home_javascript(request):
+    return render(request, 'index_javascript.html')
+
+
+def pesquisa_javascript(request, nome):
+    # Aqui eu pego todos os cursos para começarmos a trabalhar
+    cursos = Curso.objects.all()
+
+    # Atribuo a variavel 'curso_pesquisado' o valor pesquisado no formulário e está na url
+    # Neste caso, recebo diretamente como parâmetro
+    curso_pesquisado = nome
 
     # Se existir o valor na pesquisa
     if curso_pesquisado is not None:
